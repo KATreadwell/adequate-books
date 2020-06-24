b1b85fb45124c05aac6f0db175d3170607126de4
KAT <katreadwell1@gmail.com>
2020-06-23 23:32:03 -0700

    initial setup with thanks to Diarmuid!

---
+++ client/build/precache-manifest.b0f457c3515f6f82de93e492d3c169fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511b104a73d1e2b98e0e31400775ec54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a977a2be166ba606231",
+    "url": "/static/css/main.55a7a22d.chunk.css"
+  },
+  {
+    "revision": "28d8f4408f31249af37f",
+    "url": "/static/js/2.5723787b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5723787b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a977a2be166ba606231",
+    "url": "/static/js/main.15886526.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const db = require("./models/Book");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost/adequate-books", {
+  useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("/test", (req, res) => {
+  db.Book.create({
+    title: "title",
+    authors: ["authors", "2nd author"],
+    description: "stuff",
+    link: "google.com",
+    image: "stuff.jpg"
+  }).then(() => res.send("response"))
+} )
 
 // Send every request to the React app
 // Define any API routes before this runs
